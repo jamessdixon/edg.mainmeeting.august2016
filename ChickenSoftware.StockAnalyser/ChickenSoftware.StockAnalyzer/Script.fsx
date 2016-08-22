9b1009fb5d4de3edf679bfbb369cc5d8d0e1c5e9
Jamie Dixon <jamessdixon@gmail.com>
2016-08-22 17:25:50 -0400

    Initial commit

---
+++ ChickenSoftware.StockAnalyser/ChickenSoftware.StockAnalyzer/Script.fsx
@@ -0,0 +1,62 @@
+﻿
+#r "../packages/FSharp.Data.2.3.0/lib/net40/FSharp.Data.dll"
+open FSharp.Data
+
+type YahooContext = CsvProvider<"http://ichart.finance.yahoo.com/table.csv?s=MSFT">
+let stockInfo = YahooContext.Load("http://ichart.finance.yahoo.com/table.csv?s=MSFT")
+let mostRecent = stockInfo.Rows |> Seq.head
+(float)mostRecent.``Adj Close``
+
+#load "../packages/FSharp.Charting.0.90.14/FSharp.Charting.fsx"
+open System
+open FSharp.Charting
+
+let rows = stockInfo.Rows
+rows 
+|> Seq.map(fun r -> r.Date, (float)r.``Adj Close``)
+|> Chart.FastLine
+
+
+#r "../packages/Accord.3.0.2/lib/net40/Accord.dll"
+#r "../packages/Accord.Statistics.3.0.2/lib/net40/Accord.Statistics.dll"
+#r "../packages/Accord.Math.3.0.2/lib/net40/Accord.Math.dll"
+open Accord
+open Accord.Statistics
+open Accord.Statistics.Models.Regression.Linear
+
+let x = 
+    rows
+    |> Seq.take(20) 
+    |> Seq.map(fun r -> r.Date.ToOADate()) 
+    |> Seq.toArray
+let y = 
+    rows 
+    |> Seq.take(20) 
+    |> Seq.map(fun r -> (float)r.``Adj Close``) 
+    |> Seq.toArray
+
+let regression = SimpleLinearRegression()
+let sse = regression.Regress(x,y)
+let mse = sse/float x.Length 
+let rmse = sqrt(mse)
+let r2 = regression.CoefficientOfDetermination(x,y)
+
+let nextDate = [|(new DateTime(2016,5,11)).ToOADate()|]
+regression.Compute(nextDate)
+
+#r "System.Net.Http.dll"
+#r "System.Web.dll"
+open System.Net.Http
+type LuisContext = JsonProvider<"../data/Luis.json">
+
+let client = new HttpClient()
+let appId = "4015e314-326d-4443-a3ad-854f37352e3d"
+let subscriptionKey = "847c177472014d77924e14b82600f35e"
+let queryString = System.Web.HttpUtility.UrlEncode("how much are ibm stocks")
+let uri = "https://api.projectoxford.ai/luis/v1/application?id=" + appId + "&subscription-key=" + subscriptionKey + "&q=" + queryString
+let message = client.GetAsync(uri).Result
+let response = message.Content.ReadAsStringAsync().Result
+let luis = LuisContext.Parse(response)
+let entity = luis.Entities |> Seq.head
+entity.Entity
+
---
+++ ChickenSoftware.StockAnalyser/ChickenSoftware.StockAnalyzer/StockProvider.fs
@@ -0,0 +1,61 @@
+﻿namespace ChickenSoftware.StockAnalyzer
+
+open Accord
+open System.Web
+open FSharp.Data
+open System.Net.Http
+open Accord.Statistics
+open Accord.Statistics.Models.Regression.Linear
+
+type YahooContext = CsvProvider<"http://ichart.finance.yahoo.com/table.csv?s=MSFT">
+type LuisContext = JsonProvider<"../data/Luis.json">
+
+type StockProvider() = 
+    member this.GetMostRecentClose(ticker) =
+        try
+            let stockInfo = YahooContext.Load("http://ichart.finance.yahoo.com/table.csv?s=" + ticker)
+            let mostRecent = stockInfo.Rows |> Seq.head
+            (float)mostRecent.``Adj Close``
+        with
+        | :?  System.Net.WebException -> -1.0
+            
+    member this.PredictStockPrice(ticker, nextDate:System.DateTime) =
+        try
+            let stockInfo = YahooContext.Load("http://ichart.finance.yahoo.com/table.csv?s=" + ticker)
+            let rows = stockInfo.Rows |> Seq.take(20)
+            let x = 
+                rows
+                |> Seq.map(fun r -> r.Date.ToOADate()) 
+                |> Seq.toArray
+            let y = 
+                rows 
+                |> Seq.map(fun r -> (float)r.``Adj Close``) 
+                |> Seq.toArray
+
+            let regression = SimpleLinearRegression()
+            regression.Regress(x,y) |> ignore
+            let nextDate' = [|nextDate.ToOADate()|]
+            regression.Compute(nextDate') |> Seq.head
+        with
+        | :?  System.Net.WebException -> -1.0
+
+    member this.GetTicker(phrase:string) =
+        try
+            use client = new HttpClient()
+            let appId = "4015e314-326d-4443-a3ad-854f37352e3d"
+            let subscriptionKey = "847c177472014d77924e14b82600f35e"
+            let queryString = System.Web.HttpUtility.UrlEncode(phrase)
+            let uri = "https://api.projectoxford.ai/luis/v1/application?id=" + appId + "&subscription-key=" + subscriptionKey + "&q=" + queryString
+            use message = client.GetAsync(uri).Result
+            let response = message.Content.ReadAsStringAsync().Result
+            let luis = LuisContext.Parse(response)
+            if((luis.Entities |> Seq.length) > 0) then
+                let entity = luis.Entities |> Seq.head
+                if((float)entity.Score > 0.50) then entity.Entity
+                else System.String.Empty
+            else
+                System.String.Empty
+        with
+        | :?  System.Net.WebException -> System.String.Empty
+
+        
